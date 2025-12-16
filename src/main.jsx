cc87b8fbdba756025724442bec5cbe34c9e5283a
arianna <ariapazateran@crackthecode.la>
2025-12-16 16:41:35 -0500

    segundo commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter,HashRouter } from "react-router-dom";
 import App from "./App";                  // 👈 importante esta ruta
 import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
